0abb21a19354c2adbb3abb21c29715a6355aa0f9
John Knotts <jhnnyk@gmail.com>
2018-07-27 18:27:07 -0600

    removing crap from A2Hosting setup

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-const port = process.env.PORT || 52525;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
